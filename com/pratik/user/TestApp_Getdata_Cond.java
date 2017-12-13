2565297d342bc6c01e8fcb068785da7d0a9d6660
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-13 06:00:00 +0530

    TestApp_Getdata_Conditions program

---
+++ com/pratik/user/TestApp_Getdata_Cond.java
@@ -0,0 +1,23 @@
+package com.pratik.user;
+
+import com.pratik.DAO.DAO_Factory;
+import com.pratik.DAO.DAO_Interface;
+import com.pratik.domain.Jobs;
+import com.pratik.domain.person;
+
+public class TestApp_Getdata_Cond {
+
+	public static void main(String[] args) {
+		//create session
+
+		Jobs j=null;
+		person p=null;
+		
+		//use the DAO
+		DAO_Interface dao=null;
+		dao=DAO_Factory.getInstance();
+		
+		dao.getdataCondition();
+	}
+
+}
